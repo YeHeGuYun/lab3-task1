2476f574ef49e574487e5ba3fed21d937148dc76
YeHeGuYun <124816982+YeHeGuYun@users.noreply.github.com>
2024-03-11 07:56:57 +0000

    some changes

--- src/student.c
+++ src/student.c
@@ -7,7 +7,9 @@
 void sstudent (void) {
 /* student is the variable of stucture StudentData*/
 struct StudentData student;
-/* Assinging the values to StudentData*/ student.stu_name = "Zhiwen Zheng"; student.stu_id = 123456;
+/* Assinging the values to StudentData*/ 
+student.stu_name = "Zhiwen Zheng";
+student.stu_id = 123456;
 student.stu_age = 20;
 /* new structure */
 student.stuAddress.street = "Заславская 12";
@@ -15,5 +17,6 @@ student.stuAddress.state = "Central"; student.stuAddress.city = "Minsk"; student
 /* Displaying the values of struct memeber */
 printf ("Hello, %s!", student.stu_name); printf("\nStudent Id is: %d", student.stu_id); printf("\nStudent Age is: %d", student.stu_age); printf("\n Country %s, Regin %s, City %s, Address
 %s", student.stuAddress.country, student.stuAddress.state, student.stuAddress.city, student.stuAddress.street);
-printf ("\nGoodbye!"); return 0;
+printf ("\nGoodbye!"); 
+return 0;
 }
